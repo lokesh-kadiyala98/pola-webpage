3baaea9108b46d6f6ad61431f6ef4cfa6832bd93
lokesh-kadiyala98 <33837730+lokesh-kadiyala98@users.noreply.github.com>
2019-07-08 12:09:28 +0530

    "refactor: footer section"

--- src/components/footerComponent.jsx
+++ src/components/footerComponent.jsx
@@ -4,9 +4,21 @@ const footerComponent = () => {
 
     return(
         <footer>
-            <p>
-                This webpage was handcrafted by CSE students, RYMEC &copy; 2019. All rights reserved.
-            </p>
+            <div className="row">
+                <div className="col-xs-12 col-md-6 offset-sm-6">
+                    <ul className="social-links">
+                        <li><i className="fab fa-react"></i></li>
+                        <li><i className="fab fa-node-js"></i></li>
+                        <li><i className="fab fa-js"></i></li>
+                        <li><i className="fab fa-github"></i></li>
+                    </ul>
+                </div>
+            </div>
+            <div className="row">
+                <p className="col-xs-12">
+                    This webpage was handcrafted by CSE students, RYMEC &copy; 2019. All rights reserved.
+                </p>
+            </div>
         </footer>
     )
 
